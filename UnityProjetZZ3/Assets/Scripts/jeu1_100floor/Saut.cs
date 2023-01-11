6718f8681b66c27c27aa3445ad63e9b8f8faab5d
MATHVDT <mathieu.detorcy@gmail.com>
2023-01-11 23:13:12 +0100

    Modif palteforme saut
    Collider et force impulse saut par animation et en un seul objet
    Normalement temps frame  animation plateforme saut ok

--- UnityProjetZZ3/Assets/Scripts/jeu1_100floor/Saut.cs
+++ UnityProjetZZ3/Assets/Scripts/jeu1_100floor/Saut.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Saut : MonoBehaviour
 {
     private float _forceSaut;
+    private Rigidbody2D _rbPlayer;
+
 
     private void Start()
     {
-        _forceSaut = GetComponentInParent<PlateformeSaut>().ForceSaut;
-
+        _forceSaut = GetComponent<PlateformeSaut>().ForceSaut;
     }
 
 
@@ -19,17 +20,20 @@ public class Saut : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // TODO A virer apres, juste pour les test
         _forceSaut = GetComponentInParent<PlateformeSaut>().ForceSaut;
 
     }
 
-   
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            //collision.gameObject.GetComponent<Player>().Yvelocity = _forceSaut;
-        }
+        //if (this.enabled && collision.gameObject.CompareTag("Player"))
+        //{
+        //    Debug.Log("Saut");
+        //    collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, _forceSaut), ForceMode2D.Impulse);
+        //    this.enabled = false;
+        //}
     }
 }
