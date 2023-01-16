b5a56539c535b23efdc0283fe9b59651d7785efe
MATHVDT <72647750+MATHVDT@users.noreply.github.com>
2023-01-16 13:09:29 +0100

    Récupération des coord min et max de la carte ok

--- UnityProjetZZ3/Assets/Scripts/jeu1_100floor/ContourCarte.cs
+++ UnityProjetZZ3/Assets/Scripts/jeu1_100floor/ContourCarte.cs
@@ -10,43 +10,28 @@ public class ContourCarte : MonoBehaviour
     public float YMinContourCarte;
     public float YMaxContourCarte;
 
-    public Transform[] ContoursCarte;
-
     // Start is called before the first frame update
     void Start()
+    {
+
+        SetCoordMinMaxContourCarte();
+    }
+
+
+    public void SetCoordMinMaxContourCarte()
     {
         // Get position bord écran
         float objectWidth, objectHeight;
 
-        ContoursCarte = gameObject.GetComponentsInChildren<Transform>();
+        var collider = gameObject.GetComponent<BoxCollider2D>();
 
-        foreach (Transform child in ContoursCarte)
-        {
-            var collider = child.GetComponent<BoxCollider2D>();
-            if (collider == null)
-                continue;
+        objectWidth = collider.size.x; // récupération de la largeur de l'objet
+        objectHeight = collider.size.y; // récupération de la hauteur de l'objet
 
-            objectWidth = collider.size.x + child.transform.lossyScale.x; // récupération de la largeur de l'objet
-            objectHeight = collider.size.y + child.transform.lossyScale.y; // récupération de la hauteur de l'objet
+        XMinContourCarte = (transform.position.x + collider.offset.x - (objectWidth / 2));
+        XMaxContourCarte = (transform.position.x + collider.offset.x + (objectWidth / 2));
 
-            switch (child.name)
-            {
-                case "MurCarteGauche":                    // Limite gauche
-                    XMinContourCarte = (child.transform.position + (objectWidth / 2) * child.transform.right).x;
-                    break;
-                case "MurCarteDroite":                    // Limite droite
-                    XMaxContourCarte = (child.transform.position - (objectWidth / 2) * child.transform.right).x;
-                    break;
-                case "MurCarteHaut":                    // Limite haut
-                    YMinContourCarte = (child.transform.position + (objectHeight / 2) * child.transform.up).y;
-                    break;
-                case "MurCarteBas":                    // Limite bas
-                    YMaxContourCarte = (child.transform.position - (objectHeight / 2) * child.transform.up).y;
-                    break;
-                default:
-                    Debug.Log(child.name); break;
-
-            }
-        }
+        YMinContourCarte = (transform.position.y + collider.offset.y - (objectHeight / 2));
+        YMaxContourCarte = (transform.position.y + collider.offset.y + (objectHeight / 2));
     }
 }
