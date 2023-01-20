18486f591efa7ac045cdb86dca69777f1b604af0
mathvdt <mathieu.detorcy@gmail.com>
2023-01-20 14:43:41 +0100

    Ajout de la désactivation du collider quand plateforme au dessus du player
    Peut etre pb pour les plateformes avec pics

--- UnityProjetZZ3/Assets/Scripts/jeu1_100floor/MovePlateforme.cs
+++ UnityProjetZZ3/Assets/Scripts/jeu1_100floor/MovePlateforme.cs
@@ -7,9 +7,29 @@ public class MovePlateforme : MonoBehaviour
     // Vitesse de déplacement de l'objet (en unités par seconde)
     public float Speed;
 
+    private Collider2D[] _colliders2D;
+    private Transform _playerTransform;
+
+    public void Start()
+    {
+        _colliders2D = GetComponentsInChildren<Collider2D>();
+        _playerTransform = GameObject.Find("Player").transform;
+    }
+
     void Update()
     {
         // On déplace l'objet en utilisant la vitesse et le temps écoulé depuis le dernier frame
         transform.position += Vector3.up * Speed * Time.deltaTime;
+
+        if(transform.position.y > _playerTransform.position.y)
+            DesactivateColliders2D();
+
+    }
+
+    private void DesactivateColliders2D()
+    {
+        foreach(var collider in _colliders2D) {
+            collider.isTrigger = true;
+        }
     }
 }
