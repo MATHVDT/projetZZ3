855f5c6ebd76da83e1d319bd4b0cf954f7623b55
DesnosNathan <desnos.nathan@gmail.com>
2023-03-03 12:32:41 +0100

    Affichage de la carte et déplacement du joeur sur celle-ci fonctionnels

--- UnityProjetZZ3/Assets/Scripts/jeu2_BP/PlayerScript.cs
+++ UnityProjetZZ3/Assets/Scripts/jeu2_BP/PlayerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine;
 
@@ -9,14 +10,19 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D _rb;
     private SetValueControls _controls;
     private Vector2 _playerVelocity;
+    private int _maxBombs;
+    public int _currentBombs;
 
     public float MovementSpeed;
+    public GameObject _bombPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         _initialPosition = transform.position;
         _rb = GetComponent<Rigidbody2D>();
+        _maxBombs = 1;
+        _currentBombs = _maxBombs;
 
         _controls = GameObject.Find("[UI] Controls").GetComponent<SetValueControls>();
     }
@@ -30,6 +36,19 @@ public class PlayerScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (_controls.buttonA && _currentBombs > 0)
+        {
+            _currentBombs--;
+
+            GameObject bomb = Instantiate(_bombPrefab);
+            Vector3 bombPosition = transform.position + new Vector3(0, - transform.localScale.y / 4, 0);
+
+            //On ajuste les coordonnées de la bombe pour q'uelle se trouve exactement sur une case de la grille
+            bomb.transform.position = new Vector3(Mathf.Floor(bombPosition.x) + (float)0.5,
+                                                (Mathf.Floor(bombPosition.y) + (float)0.5) * (float)0.7,
+                                                Mathf.Floor(bombPosition.z) + (float)0.5);
+            bomb.SetActive(true);
+        }
 
         _playerVelocity = _rb.velocity;
         _playerVelocity.x = _controls.horizontalAxis * MovementSpeed;
